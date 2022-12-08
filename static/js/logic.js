06153fbd830b5ddfec311a4ff99ada21a9571ecd
Dustin Combs <dccombs@gmail.com>
2022-12-08 12:43:39 -0500

    testing

--- static/js/logic.js
+++ static/js/logic.js
@@ -11,7 +11,7 @@ function makeMap(features){
     );
   }
   
-  var token = "pk.eyJ1IjoiZGNjb21icyIsImEiOiJja2lydWVwbnkwYWg4MzVvMXJmaDhieGxrIn0.LRVQLFWbFhAjvhfCUBwjAA"
+  var token = "pk.eyJ1IjoiZGNjb21icyIsImEiOiJja2lydWVwbnkwYWg4MzVvMXJmaDhieGxrIn0.LRVQLFWbFhAjvhfCUBwjAA";
   // create dark map
   var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -20,7 +20,7 @@ function makeMap(features){
     maxZoom: 12,
     zoomOffset: -1,
     id: "mapbox/dark-v10",
-    accessToken: 
+    accessToken: token
   });
 
   // create light map
